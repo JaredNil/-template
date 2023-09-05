16f1c1359c2aea16077bcc7d04118cef9504277e
JN <jared.bozh@gmail.com>
2023-09-06 01:37:27 +0800

    add scss arch and arch of web common

--- architect/web-api.ts
+++ architect/web-api.ts
@@ -1,2 +1,6 @@
 // https://developer.mozilla.org/ru/docs/Web/API
 // Перечислить основные интерфейсы из WebAPI
+
+
+// user-agent - ?
+window.navigator.userAgent // -> 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 YaBrowser/23.7.3.823 Yowser/2.5 Safari/537.36'
